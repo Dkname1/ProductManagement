a9a8cf66f3672ae967f70011a13c1942da79ead0
Dkname1 <115562828+Dkname1@users.noreply.github.com>
2024-01-18 15:31:00 +0530

    Add files via upload

---
+++ Product_Management_Backend/src/main/java/com/becoder/ProductManagementBackendApplication.java
@@ -0,0 +1,15 @@
+package com.becoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductManagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductManagementBackendApplication.class, args);
+		
+		System.out.println("Program run.....");
+	}
+
+}
